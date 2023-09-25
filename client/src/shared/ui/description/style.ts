472981fdf993c9a62584b00ee0061807cb2d4acb
Your Name <you@example.com>
2023-09-25 12:10:22 +0300

    product article code splitting + fix + small cms types settings + readme.md update

---
+++ client/src/shared/ui/description/style.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Style = styled.div`
+    background: white;
+    border-radius: 12px;
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+    width: fit-content;
+`;
