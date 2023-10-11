a228879406c25c268f393103586ddd60735939b3
nofancy <aleop00040005@gmail.com>
2023-10-11 13:10:36 +0300

    build upload

--- client/src/shared/api/index.ts
+++ client/src/shared/api/index.ts
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 const appApi = createApi({
-    baseQuery: fetchBaseQuery({ baseUrl: 'http://127.0.0.1:1337/api' }),
+    baseQuery: fetchBaseQuery({ baseUrl: 'http://192.168.88.25:1337/api' }),
     endpoints: () => ({}),
 })
 export default appApi
--- server/main.py
+++ server/main.py
@@ -24,4 +24,4 @@ app.add_middleware(
 buildDir = str(os.path.abspath(os.getcwd()))+"/server/dist"
 app.mount("/", StaticFiles(directory=buildDir, html=True), name="dist")
 # email = Email(app, "***", "***", "***")
-uvicorn.run(app, host='0.0.0.0', port=5000, log_level='info')
+uvicorn.run(app, host='0.0.0.0', port=5000, log_level='info', workers=True)
